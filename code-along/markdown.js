39c51e9868f251cf676ab6f35924ca88f01ebdee
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2019-12-11 15:11:32 +0100

    promisify-code-along

--- code-along/markdown.js
+++ code-along/markdown.js
@@ -1,92 +1,146 @@
 // make it only call snippet when iframe is in view?
+// either make a back button when links are followed, or have them open in new tabs
 
-// async/await / promisify this
+
+
 async function codeAlong(config) {
-  const markdown = await fetch(config.path)
-    .then(resp => resp.text())
-    .catch(err => console.log(err))
-    .then(parsedMd => parsedMd)
-    .catch(err => {
-      console.log(err)
-      return '```error loading example```';
-    });
+
 
   const container = config.container instanceof Element
     ? config.container
-    : document.getElementById(config.container);
-  setTimeout(() => {
-
-    const iframe = document.createElement('iframe');
-    iframe.style = 'height:100%;width:100%;';
-    iframe.setAttribute('scrolling', 'no');
-
-    iframe.onload = () => {
-
-      const frameContext = iframe.contentWindow;
+    : typeof config.container === 'string'
+      ? document.getElementById(config.container)
+      : document.createElement('div');
 
 
-      ((document) => {
 
-        const ghMdStyle = document.createElement('link');
-        ghMdStyle.type = 'text/css';
-        ghMdStyle.setAttribute('rel', 'stylesheet');
-        ghMdStyle.setAttribute('href', './code-along/github-markdown.css');
-        document.head.appendChild(ghMdStyle);
+  const markdown = await fetch(config.path)
+    .then(resp => resp.text())
+    .then(parsedMd => parsedMd)
+    .catch(err => {
+      console.log(err);
+      return err;
+    });
 
-        const markedScript = document.createElement('script');
-        markedScript.src = "./code-along/marked.js";
-        markedScript.type = "text/javascript";
-        markedScript.charset = "utf-8";
+  if (markdown instanceof Error) {
+    const errorEl = document.createElement('code');
+    errorEl.innerHTML = markdown.name + ': ' + markdown.message;
+    container.appendChild(errorEl);
+    return container;
+  }
 
-        document.head.appendChild(markedScript);
+  const iframe = document.createElement('iframe');
+  iframe.style = 'height:100%;width:100%;';
+  iframe.setAttribute('scrolling', 'no');
+
+  iframe.onload = async () => {
+    // const buildCodeAlong = async () => {
+
+    const frameDocument = iframe.contentWindow.document;
+    const frameWindow = iframe.contentWindow;
+
+    const loadingMdStyles = new Promise((resolve, reject) => {
+      const ghMdStyle = document.createElement('link');
+      ghMdStyle.type = 'text/css';
+      ghMdStyle.setAttribute('rel', 'stylesheet');
+      ghMdStyle.setAttribute('href', './code-along/github-markdown.css');
+
+      ghMdStyle.addEventListener('load', () => resolve());
+      ghMdStyle.addEventListener('error', (e) => reject(e.message))
+
+      frameDocument.head.appendChild(ghMdStyle);
+    });
+
+    const loadingMarked = new Promise((resolve, reject) => {
+      const markedScript = document.createElement('script');
+      markedScript.src = "./code-along/marked.js";
+      markedScript.type = "text/javascript";
+      markedScript.charset = "utf-8";
+
+      markedScript.addEventListener('load', () => resolve());
+      markedScript.addEventListener('error', (e) => reject(e.message))
+
+      frameDocument.head.appendChild(markedScript);
+    });
+
+    const loadingAce = new Promise((resolve, reject) => {
+      const aceScript = document.createElement('script');
+      aceScript.src = "./code-along/ace/ace.js";
+      aceScript.type = "text/javascript";
+      aceScript.charset = "utf-8";
+
+      aceScript.addEventListener('load', () => resolve());
+      aceScript.addEventListener('error', (e) => reject(e.message))
+
+      frameDocument.head.appendChild(aceScript);
+    });
 
 
-        const aceScript = document.createElement('script');
-        aceScript.src = "./code-along/ace/ace.js";
-        aceScript.type = "text/javascript";
-        aceScript.charset = "utf-8";
+    await Promise.all([loadingMdStyles, loadingMarked, loadingAce])
+      .then(function () {
 
-        document.head.appendChild(aceScript);
+        const ace = frameWindow.ace;
+        const marked = frameWindow.marked;
 
+        const editorDiv = document.createElement('div');
+        editorDiv.style = 'height:70vh;width:60vw;';
 
-        // promisify this setTimeout
-        setTimeout(() => {
+        const editor = ace.edit(editorDiv);
 
-          const ace = frameContext.ace;
-          const marked = frameContext.marked;
+        editor.setTheme('ace/theme/twilight');
+        editor.setFontSize(15);
+        editor.getSession().setMode('ace/mode/markdown');
+        editor.getSession().setTabSize(2);
+        editor.setValue(markdown);
 
-          const editorDiv = document.createElement('div');
-          editorDiv.style = 'height:70vh;width:60vw;';
+        const outputDiv = document.createElement('div');
+        outputDiv.id = '\n-- study: markdown rendered as DOM --\n';
+        outputDiv.className = 'markdown-body';
+        outputDiv.style = 'height: 69vh; width: 40vw; border:solid; padding-left:3%; padding-right:3%;';
+        outputDiv.innerHTML = marked(editor.getValue());
 
-          const editor = ace.edit(editorDiv);
-
-          editor.setTheme('ace/theme/twilight');
-          editor.setFontSize(15);
-          editor.getSession().setMode('ace/mode/markdown');
-          editor.getSession().setTabSize(2);
-          editor.setValue(markdown);
-
-          const outputDiv = document.createElement('div');
-          outputDiv.id = '\n-- study: markdown rendered as DOM --\n';
-          outputDiv.className = 'markdown-body';
-          outputDiv.style = 'height: 69vh; width: 40vw; border:solid; padding-left:3%; padding-right:3%;';
+        editor.on("change", () => {
           outputDiv.innerHTML = marked(editor.getValue());
+        });
 
-          editor.on("change", (e) => {
-            outputDiv.innerHTML = marked(editor.getValue());
-          });
+        frameDocument.body.style = 'display:flex; flex-direction:row;';
+        frameDocument.body.appendChild(editorDiv);
+        frameDocument.body.appendChild(outputDiv);
 
-          document.body.style = 'display:flex; flex-direction:row;';
-          document.body.appendChild(editorDiv);
-          document.body.appendChild(outputDiv);
+      })
+      .catch(err => {
+        console.log(err);
+        frameDocument.innerHTML = ''
+        const errorEl = document.createElement('code');
+        errorEl.innerHTML = markdown.name + ': ' + markdown.message;
+        frameDocument.appendChild(errorEl);
+      });
 
-        }, 500);
 
-      })(frameContext.document)
+  }
 
-    }
+  // // later
+  // let built = false;
+  // const observer = new IntersectionObserver(() => {
+  //   if (!built) {
+  //     buildCodeAlong();
+  //     built = true;
+  //   }
+  // });
+  // observer.observe(iframe);
 
-    container.appendChild(iframe);
-  }, 500)
+  container.appendChild(iframe);
+
+  return container;
+
 
 }
+
+
+
+  // frameContext.addEventListener('popstate',
+  //   (function goBack(confirmed) {
+  //     return confirmed
+  //       ? window.history.back()
+  //       : goBack(confirm('go back?'))
+  //   })(confirm('go back?')));
