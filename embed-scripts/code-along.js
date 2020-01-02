2e9c9a6ff9403907c7adbf644b0c56d912316336
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2020-01-02 19:52:42 +0100

    dead-links

--- embed-scripts/code-along.js
+++ embed-scripts/code-along.js
@@ -30,7 +30,6 @@ async function codeAlong(config) {
   const iframe = document.createElement('iframe');
   iframe.style = 'height:100vh;width:100%;overflow:hidden;background-color:white;';
   iframe.setAttribute('scrolling', 'no');
-  // iframe.setAttribute("allowtransparency", false);
 
   iframe.onload = async () => {
     // const buildCodeAlong = async () => {
@@ -80,46 +79,36 @@ async function codeAlong(config) {
 
 
     await Promise.all([loadingMdStyles, loadingMarked, loadingAce])
-      .then(function () {
 
-        const ace = frameWindow.ace;
-        const marked = frameWindow.marked;
+    const ace = frameWindow.ace;
+    const marked = frameWindow.marked;
 
-        const editorDiv = document.createElement('div');
-        // editorDiv.style = 'height:70vh;width:60vw;';
-        editorDiv.style = 'height:90vh;width:100vw;';
+    const editorDiv = document.createElement('div');
+    // editorDiv.style = 'height:70vh;width:60vw;';
+    editorDiv.style = 'height:90vh;width:100vw;';
 
-        const editor = ace.edit(editorDiv);
+    const editor = ace.edit(editorDiv);
 
-        editor.setTheme('ace/theme/twilight');
-        editor.setFontSize(15);
-        editor.getSession().setMode('ace/mode/markdown');
-        editor.getSession().setTabSize(2);
-        editor.setValue(markdown);
+    editor.setTheme('ace/theme/twilight');
+    editor.setFontSize(15);
+    editor.getSession().setMode('ace/mode/markdown');
+    editor.getSession().setTabSize(2);
+    editor.setValue(markdown);
 
-        const outputDiv = document.createElement('div');
-        outputDiv.id = '\n-- study: markdown rendered as DOM --\n';
-        outputDiv.className = 'markdown-body';
-        // outputDiv.style = 'height: 69vh; width: 40vw; border:solid; padding-left:3%; padding-right:3%;';
-        outputDiv.style = 'height: 90vh; width: 100%; border:solid 2px; padding-left:3%; padding-right:3%;';
-        outputDiv.innerHTML = marked(editor.getValue());
+    const outputDiv = document.createElement('div');
+    outputDiv.id = '\n-- study: markdown rendered as DOM --\n';
+    outputDiv.className = 'markdown-body';
+    // outputDiv.style = 'height: 69vh; width: 40vw; border:solid; padding-left:3%; padding-right:3%;';
+    outputDiv.style = 'height: 90vh; width: 100%; border:solid 2px; padding-left:3%; padding-right:3%;';
+    outputDiv.innerHTML = marked(editor.getValue());
 
-        editor.on("change", () => {
-          outputDiv.innerHTML = marked(editor.getValue());
-        });
+    editor.on("change", () => {
+      outputDiv.innerHTML = marked(editor.getValue());
+    });
 
-        frameDocument.body.style = 'display:flex; flex-direction:row;';
-        frameDocument.body.appendChild(editorDiv);
-        frameDocument.body.appendChild(outputDiv);
-
-      })
-      .catch(err => {
-        console.log(err);
-        frameDocument.innerHTML = ''
-        const errorEl = document.createElement('code');
-        errorEl.innerHTML = markdown.name + ': ' + markdown.message;
-        frameDocument.appendChild(errorEl);
-      });
+    frameDocument.body.style = 'display:flex; flex-direction:row;';
+    frameDocument.body.appendChild(editorDiv);
+    frameDocument.body.appendChild(outputDiv);
 
 
   }
